60945ff39ab7ff951df5735578cbac1b1fc72062
Dimitry Vashkevich <dimalepel@mail.ru>
2023-08-25 20:40:06 +0300

    feat: add bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> categoriesPreview = [
+    'assets/images/category_1.png',
+    'assets/images/category_2.png',
+    'assets/images/category_3.png',
+    'assets/images/category_4.png',
+    'assets/images/category_1.png',
+    'assets/images/category_2.png',
+    'assets/images/category_3.png',
+    'assets/images/category_4.png'
+  ];
+
+  final List<String> categoriesTitle= [
+    'Пекарни и кондитерские ',
+    'Фастфуд',
+    'Азиатская кухня',
+    'Супы',
+    'Пекарни и кондитерские ',
+    'Фастфуд',
+    'Азиатская кухня',
+    'Супы'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -61,6 +88,228 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, right: 16, left: 16),
+        child: ListView.builder(
+            itemCount: categoriesTitle.length,
+            itemBuilder: (_, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Container(
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          categoriesPreview[index],
+                          fit: BoxFit.cover,
+                          height: 148,
+                          width: MediaQuery.of(context).size.width,
+                        ),
+                      ),
+                      Positioned(
+                          left: 16,
+                          top: 12,
+                          child: SizedBox(
+                            width: 190,
+                            child: Text(
+                              categoriesTitle[index],
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
+                            ),
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedIconTheme: IconThemeData(
+          color: Color(0xff3364E0),
+        ),
+        useLegacyColorScheme: false,
+        unselectedLabelStyle: TextStyle(
+          color: Color(0xffA5A9B2),
+          fontSize: 12,
+          fontWeight: FontWeight.w500
+        ),
+        selectedItemColor: Color(0xff3364E0),
+        selectedLabelStyle: TextStyle(
+          color: Color(0xff3364E0),
+            fontSize: 12,
+            fontWeight: FontWeight.w500
+        ),
+        showUnselectedLabels: true,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Color(0xffA5A9B2),
+            ),
+            label: 'Главная',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: Color(0xffA5A9B2),
+            ),
+            label: 'Поиск',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Color(0xffA5A9B2),
+            ),
+            label: 'Корзина',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle_outlined,
+              color: Color(0xffA5A9B2),
+            ),
+            label: 'Аккаунта',
+          ),
+        ],
+      ),
+
+      /*SingleChildScrollView(
+        child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                children: [
+                  Container(
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            categoriesPreview[0],
+                            fit: BoxFit.cover,
+                            height: 148,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ),
+                        Positioned(
+                          left: 16,
+                          top: 12,
+                          child: SizedBox(
+                            width: 190,
+                            child: Text(
+                              categoriesTitle[0],
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
+                            ),
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            categoriesPreview[1],
+                            fit: BoxFit.cover,
+                            height: 148,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ),
+                        Positioned(
+                            left: 16,
+                            top: 12,
+                            child: SizedBox(
+                              width: 190,
+                              child: Text(
+                                categoriesTitle[1],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            categoriesPreview[2],
+                            fit: BoxFit.cover,
+                            height: 148,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ),
+                        Positioned(
+                            left: 16,
+                            top: 12,
+                            child: SizedBox(
+                              width: 190,
+                              child: Text(
+                                categoriesTitle[2],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            categoriesPreview[3],
+                            fit: BoxFit.cover,
+                            height: 148,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ),
+                        Positioned(
+                            left: 16,
+                            top: 12,
+                            child: SizedBox(
+                              width: 190,
+                              child: Text(
+                                categoriesTitle[3],
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+      ),*/
     );
   }
 }
